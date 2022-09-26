256f6d06b653c852069083cbe723f7a50a4df33d
IlyaGoncharovY <goncharov.i@rambler.ru>
2022-09-27 00:08:30 +0300

    refactor preloader

--- src/Components/common/Preloader/Preloader.tsx
+++ src/Components/common/Preloader/Preloader.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import s from "./preloader.module.css";
 import preloader from "../../../assecs/image/1485.gif";
+import Box from "@mui/material/Box";
+import {LinearProgress} from "@mui/material";
 
 export const Preloader = () => {
-    return <div className={s.fetchAnimation}><img src={preloader}/></div>
+    return (
+        <Box sx={{width: '100%'}}>
+            <LinearProgress/>
+        </Box>
+    );
 };
 
